8a3e962c78bf24c7252130e3eae0fdc764b41d66
Miguel Cardoso <mi_lmc@hotmail.com>
2022-05-27 15:45:34 -0300

    bcrypt include

--- users/usersController.js
+++ users/usersController.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./User')
+const bcrypt = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
+
 
 // USERS LIST
-router.get('/admin/user',(req,res)=>{
-   res.send('listagem de users') 
+router.get('/admin/users',(req,res)=>{
+    User.findAll().then(users=>{
+        res.render('./admin/users/index',{
+            users: users
+        })
+    })
+   
 })
 // CREATE USER VIEW
-router.get('/admin/user/create',(req,res)=>{
+router.get('/admin/users/create',(req,res)=>{
     res.render('admin/users/create')
 
 })
@@ -16,6 +24,21 @@ router.post('/user/create',(req,res)=>{
     let email = req.body.email;
     let password = req.body.password;
 
-    res.json({email,password})
+    User.findOne({where:{email:email}}).then(user=>{
+        if(user == undefined){
+            let salt = bcrypt.genSaltSync(10)
+            let hash = bcryptjs.hashSync(password, salt)
+        
+            User.create({
+                email: email,
+                password: hash
+            }).then(()=>{
+                res.redirect('/')
+            }).catch(()=> res.redirect('/'))
+        }else{
+            res.redirect('/admin/user/create')
+        }
+    })
+   
 })
 module.exports = router
